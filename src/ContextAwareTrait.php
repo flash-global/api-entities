111248d626be9405b6242b44824a2121be5cf684
Gauthier Delamarre <gde@opcoding.eu>
2017-08-18 14:29:14 +0200

    Remove debugging stuff

--- src/ContextAwareTrait.php
+++ src/ContextAwareTrait.php
@@ -27,7 +27,6 @@ trait ContextAwareTrait
             foreach($context as $key => $value)
             {
                 if (is_int($key)) {
-                    var_dump($context);exit;
                     throw new ContextException(sprintf('Context key must be a string, not an integer (key: "%s").', $key));
                 }
                 
